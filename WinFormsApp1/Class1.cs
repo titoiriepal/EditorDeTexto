190faf35ac0755d4241b1e5c7fb4a96af4715394
DESKTOP-LO3FN3B\TitoI <titoiriepal@gmail.com>
2021-10-09 16:29:44 +0200

    Label Creation

--- WinFormsApp1/Class1.cs
+++ WinFormsApp1/Class1.cs
@@ -20,6 +20,14 @@ public partial class Form1 : Form
         Name = "Form1";
         Text = "Saludo";
         etSaludo = new Label();
+        etSaludo.Name = "etSaludo";
+        etSaludo.Text = "etiqueta";
+        etSaludo.Font = new Font("Microsoft Sans Serif", 14, FontStyle.Regular);
+        etSaludo.TextAlign = ContentAlignment.MiddleCenter;
+        etSaludo.Location = new Point(73, 77);
+        etSaludo.Size = new Size(358, 40);
+        etSaludo.TabIndex = 1;
+        Controls.Add(etSaludo);
     }
     protected override void Dispose(bool eliminar)
     {
