3acb3d43cc036bef96c313ff700592c1e23b2693
DESKTOP-LO3FN3B\TitoI <titoiriepal@gmail.com>
2021-10-09 16:23:10 +0200

    Initial commit

---
+++ WinFormsApp1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+
+public partial class Form1 : Form
+{
+    private Label etSaludo;
+    public Form1()
+    {
+        Iniciarcomponentes();
+    }
+
+    public void Iniciarcomponentes()
+    {
+        ClientSize = new Size(505, 285);
+        Name = "Form1";
+        Text = "Saludo";
+        etSaludo = new Label();
+    }
+    protected override void Dispose(bool eliminar)
+    {
+        if (eliminar)
+        {
+
+        }
+        base.Dispose(eliminar);
+    }
+
+    public static void Main()
+    {
+        Application.Run(new Form1());
+    }
+
+}
